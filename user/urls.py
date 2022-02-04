0118fdd6c933e6e721404934f639209fca1bdc71
imanoj997 <Manoj>
2022-02-04 16:00:53 +0545

    userprofile viewset

--- user/urls.py
+++ user/urls.py
@@ -17,11 +17,13 @@ urlpatterns = [
     path("sign-in/", UserSignIn.as_view()),
     path('email_verification/<str:uidb64>/<str:token>/',
          activate_user, name='email_activate'),
-    path('facebook-sign-in/', CustomFacebookLoginView.as_view(), name='fb_sign_in'),
-    path('google-sign-in/', CustomGoogleLoginView.as_view(), name='google_sign_in'),
-    path('forgot-password-email-check/', RestPasswordConfirmEmail.as_view(),
-         name='password_reset_email_check'),
     path('password-reset/', ResetPassword.as_view(),
          name='password_reset'),
+    path('forgot-password-email-check/', RestPasswordConfirmEmail.as_view(),
+         name='password_reset_email_check'),
+
+    # remove these urls if you don't need social login
+    path('facebook-sign-in/', CustomFacebookLoginView.as_view(), name='fb_sign_in'),
+    path('google-sign-in/', CustomGoogleLoginView.as_view(), name='google_sign_in'),
     path('accounts/', include('allauth.urls'), name='socialaccount_signup'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
