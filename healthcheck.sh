f31dc08e7140cb9a570a090062b72c66dffa1eb6
Nischal Shrestha <nischalstha9@gmail.com>
2022-10-18 08:35:44 +0545

    feat: add color in healthcheck.sh

--- healthcheck.sh
+++ healthcheck.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
+RED='\033[0;31m'
+NC='\033[0m'
 
 curl http://localhost:8000 || \
-echo "HEALTHCHECK FAILURE! RESTARTING!!" && \
-bash -c 'kill -s 15 -1 && (sleep 10; kill -s 9 -1)'
+(echo "${RED}====== HEALTHCHECK FAILURE! RESTARTING!! ======${NC}" && bash -c 'kill -s 15 -1 && (sleep 10; kill -s 9 -1)')
 
