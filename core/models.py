1532eb65f0757a6e2f2233a325c04f72e7191dc9
cecililu <cecilghimire@gmail.com>
2023-03-14 13:13:11 +0545

    feat:added models for owner and Project

--- core/models.py
+++ core/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+class Owner (models.Model):
+    full_name=models.CharFeild(max_length=50)
+    mobile_number=models.IntegerField()
+    email=models.EmailField(max_length=254)
+    
+    def __str__(self):
+        return self.name
+
+
+class Project (models.Model):
+
+    name=models.CharField(max_length=50)
+    time_started=models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
