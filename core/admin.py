828bb4c121cb25313f7f4b819a3333843c3188a6
cecililu <cecilghimire@gmail.com>
2023-03-14 16:18:44 +0545

    feat:admin panel

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
-
+from . models import *
 # Register your models here.
+
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ('name','time_started')
+    # list_filter = ('time_started',)
+
+class OwnerAdmin(admin.ModelAdmin):
+    list_display=('full_name','email',)
+   
+class DepartmentAdmin(admin.ModelAdmin):
+    list_display=('name',)
+
+class OwnerProfileAdmin(admin.ModelAdmin):
+    list_display=('name','is_organization')
+    list_filter = ('is_organization',)
+
+admin.site.register(Project,ProjectAdmin)
+admin.site.register(Owner,OwnerAdmin)
+admin.site.register(Department,DepartmentAdmin)
+admin.site.register(OwnerProfile,OwnerProfileAdmin)
--- core/models.py
+++ core/models.py
@@ -1,19 +1,42 @@
 from django.db import models
+from django.dispatch import receiver
+from django.db.models.signals import post_save
 
-# Create your models here.
-class Owner (models.Model):
-    full_name=models.CharFeild(max_length=50)
+#Model Manger
+class ProjectManager(models.Manager):
+    def changeName(self,name):
+        self.name=name
+        return self.name 
+
+#Models
+class Owner(models.Model):
+    full_name=models.CharField(max_length=100)
     mobile_number=models.IntegerField()
-    email=models.EmailField(max_length=254)
-    
-    def __str__(self):
-        return self.name
+    email=models.EmailField(max_length=250) 
 
+    def __str__(self):
+        return self.full_name
 
 class Project (models.Model):
+    name=models.CharField(max_length=250)
+    time_started=models.CharField(max_length=250)
+    owner=models.ForeignKey(Owner,on_delete=models.PROTECT,null=True,blank=True)  
 
-    name=models.CharField(max_length=50)
-    time_started=models.CharField(max_length=50)
-
+    objects= ProjectManager()
     def __str__(self):
         return self.name
+
+class Department(models.Model):
+    name=models.CharField(max_length=250)
+    def __str__(self):
+        return self.name
+
+class OwnerProfile(models.Model):
+    name=models.CharField(max_length=250)
+    is_organization=models.BooleanField(default=False)
+    # description=models.CharField(max_length=50)
+@receiver(post_save, sender=Owner)
+def OwnerProfileCreator(sender, instance, **kwargs):
+    OwnerProfile(name=instance.full_name).save()
+         
+
